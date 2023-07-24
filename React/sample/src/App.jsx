624b6cd25b3a4e8fe6f80ae502841cc2b0d4210e
Sowji-dev <sowjanya.kadupu@gmail.com>
2023-07-24 08:52:48 +0530

    news app

--- React/sample/src/App.jsx
+++ React/sample/src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import News from './News';
 function App() {
-  
+  var [ar,setAr]=useState([])
+  useEffect(()=>{
+    axios.get('https://hn.algolia.com/api/v1/search?').then(res=>{
+      console.log(res.data.hits)
+      setAr([...res.data.hits])
+    })
+  },[])
   return (
     <div >
-      
+      <h3>News App</h3>
+      {
+        ar.map((e,i)=>
+          <div>
+             <News d={e}></News>
+          </div>
+        )
+      }
       </div>
   );
 }
---
+++ React/sample/src/News.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function News(props) {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default News
