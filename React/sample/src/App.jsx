5b5e41730878d054ee194ad2f9e52feab0fe7184
Sowji-dev <sowjanya.kadupu@gmail.com>
2023-07-25 15:23:29 +0530

    form

--- React/sample/src/App.jsx
+++ React/sample/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import React from 'react';
+import formik from 'module'
 function App() {
-  
+  var formik=useFormik({
+
+  })
   return (
     <div >
-      
+      <h3>Form validations using formik and yup</h3>
+      <label>User name</label>
+      <input type='text' name='myname' placeholder='Enter name' />
+
+      <label>Password</label>
+      <input type='password' name='pwd' placeholder='Enter Password' />
+
       </div>
   );
 }
