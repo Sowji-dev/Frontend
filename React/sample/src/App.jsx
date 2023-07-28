7e8061b4e3914526b43928ae66686d4f95377f9a
Sowji-dev <sowjanya.kadupu@gmail.com>
2023-07-28 09:21:33 +0530

    a

--- React/sample/src/App.jsx
+++ React/sample/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import React from 'react';
+import React, { useRef } from 'react';
 function App() {
-  
+  var ref1=useRef()
   return (
     <div >
-      
+        <h3>Calculator App</h3>
+        <input type='text' ref='ref1'/>
       </div>
   );
 }
