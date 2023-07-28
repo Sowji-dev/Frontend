d71b296030c1fb82e167aa85272273798a39f6e6
Sowji-dev <sowjanya.kadupu@gmail.com>
2023-07-28 11:50:09 +0530

    commit

--- React/sample/src/App.jsx
+++ React/sample/src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
-import React from 'react';
+import React, { useState } from 'react';
 function App() {
-  
+  var [score,setScore]=useState(0)
+  var [runs,setRuns]=useState(0)
+  function add(i){
+    setScore(score+i)
+  }
+  function addWicket(){
+    setRuns(runs+1)
+  }
   return (
     <div >
-      
+      <h4>Cricket Score:<b> {score}/{runs}</b></h4>
+      <button onClick={()=>add(0)}> 0</button>
+      <button onClick={()=>add(1)}> 1</button>
+      <button onClick={()=>add(2)}> 2</button><br/>
+      <button onClick={()=>add(3)}> 3</button>
+      <button onClick={()=>add(4)}> 4</button>
+      <button onClick={()=>add(6)}> 6</button><br/>
+      <button onClick={addWicket}> Wicket</button>
+      <button onClick={()=>add(1)}> WideBall</button>
+      <button onClick={()=>add(1)}> No Ball</button>
       </div>
   );
 }
