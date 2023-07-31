adebbdf7353f7aa8410b541504bc5b8582e8b2d1
Sowji-dev <sowjanya.kadupu@gmail.com>
2023-07-31 18:56:39 +0530

    rr

--- React/sample/src/App.jsx
+++ React/sample/src/App.jsx
@@ -1,10 +1,12 @@
+import { Outlet } from 'react-router-dom';
 import './App.css'
 import React from 'react';
 function App() {
   
   return (
     <div >
-      
+      App comp
+       
       </div>
   );
 }
--- React/sample/src/index.js
+++ React/sample/src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom/client';
  import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { createBrowserRouter } from 'react-router-dom';
+var router=createBrowserRouter([
+    {
+        path:'/',
+        element:<App></App>,
+        children:[
+            {
+                path:'/services',
+                element:<Services></Services>
+            },
+            {
+                path:'/products',
+                element:<Products></Products>
+            }
+        ]
+    }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
