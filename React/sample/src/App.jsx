050020c9cad24788387ed9b71f8c654781a8d972
Sowji-dev <sowjanya.kadupu@gmail.com>
2023-08-01 10:02:34 +0530

    added routing

--- React/sample/src/App.jsx
+++ React/sample/src/App.jsx
@@ -1,5 +1,7 @@
+import { Outlet } from 'react-router';
 import './App.css'
 import React, { useState } from 'react';
+import Sports from './Sports';
 function App() {
   var [score,setScore]=useState(0)
   var [runs,setRuns]=useState(0)
@@ -11,17 +13,21 @@ function App() {
   }
   return (
     <div >
-      <h4>Cricket Score:<b> {score}/{runs}</b></h4>
-      <button onClick={()=>add(0)}> 0</button>
-      <button onClick={()=>add(1)}> 1</button>
-      <button onClick={()=>add(2)}> 2</button><br/>
-      <button onClick={()=>add(3)}> 3</button>
-      <button onClick={()=>add(4)}> 4</button>
-      <button onClick={()=>add(6)}> 6</button><br/>
-      <button onClick={addWicket}> Wicket</button>
-      <button onClick={()=>add(1)}> WideBall</button>
-      <button onClick={()=>add(1)}> No Ball</button>
+      <div>
+        <h4>Cricket Score:<b> {score}/{runs}</b></h4>
+        <button onClick={()=>add(0)}> 0</button>
+        <button onClick={()=>add(1)}> 1</button>
+        <button onClick={()=>add(2)}> 2</button><br/>
+        <button onClick={()=>add(3)}> 3</button>
+        <button onClick={()=>add(4)}> 4</button>
+        <button onClick={()=>add(6)}> 6</button><br/>
+        <button onClick={addWicket}> Wicket</button>
+        <button onClick={()=>add(1)}> WideBall</button>
+        <button onClick={()=>add(1)}> No Ball</button>
       </div>
+      <br/><br/><br/>
+     <Outlet></Outlet>
+    </div>
   );
 }
 export default App;
--- React/sample/src/index.js
+++ React/sample/src/index.js
@@ -3,10 +3,29 @@ import ReactDOM from 'react-dom/client';
  import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Sports from './Sports';
+import Article from './Article';
+const router=createBrowserRouter([
+    {
+        path:'/',
+        element:<App></App>,
+        children:[
+            {
+                path:'/sports',
+                element:<Sports></Sports>
+            },
+            {
+                path:'/article',
+                element:<Article></Article>
+            }
+        ]
+    }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+   <RouterProvider router={router}></RouterProvider>
 );
 
 reportWebVitals();
+
