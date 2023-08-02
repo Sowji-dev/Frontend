349fe1deab8cfc08191ee02865047e495e50f09d
Sowji-dev <sowjanya.kadupu@gmail.com>
2023-08-02 15:08:46 +0530

    a

--- React/sample/src/App.jsx
+++ React/sample/src/App.jsx
@@ -1,28 +1,37 @@
 import './App.css'
-import React, { useState,useRef } from 'react';
+import React, { useState,useRef, useEffect } from 'react';
 import ShowTasks from './ShowTasks';
 function App() {
   var [ar,setAr]=useState([
     {
-        title:'car wash',
-        status:'incomplete'
+        title:'Groceries',
+        status:false
     },
     {
-        title:'insuarance payment',
-        status:'complete'
+        title:'House rent',
+        status:true
     },
     {
-        title:'rent',
-        status:'incomplete'
+        title:'Bike EMI',
+        status:false
+    },
+    {
+      title:'Tution fee',
+      status:true
+    },
+    {
+      title:'Electricity',
+      status:false
     }
 ])
 var [errmsg,setErrmsg]=useState('');
+var [showOnly,setShowOnly]=useState('all')
 function addTask(e){
   e.preventDefault()
-  if(document.myform.taskname && document.myform.status.value ){
+  if(document.myform.taskname.value ){
     ar.push({
       title: document.myform.taskname.value,
-      status: document.myform.status.value
+      status: false
     });
     setAr([...ar]);
     document.myform.reset();
@@ -31,23 +40,40 @@ function addTask(e){
   else{
      setErrmsg('Please fill task and status')
   }
-  console.dir(document.myform)
+}
+function doneTask(i){
+    ar[i].status=true;
+    setAr([...ar])
+}
+function deleteTask(i){
+  ar.splice(i,1)
+  setAr([...ar])
+}
+useEffect(()=>{
+  document.myform.status[0].checked=true
+},[])
+function changeRadio(){
+  console.dir(document.myform.status)
+  setShowOnly(document.myform.status.value)
 }
   return (
     <div >
       <br/>
       <form name='myform'>
           Task :  &nbsp;
-          <input type='text' name='taskname' placeholder='Enter task'/><br/><br/>
-          Status:  &nbsp;
-          <input type='radio'  name='status' value='complete'/> &nbsp;Completed &nbsp; &nbsp; &nbsp;
-          <input type='radio' name='status'  value='incomplete'/> &nbsp; Not Completed<br/><br/>      
+          <input type='text' name='taskname' placeholder='Enter task'/>&nbsp; &nbsp; &nbsp;
           <button onClick={addTask}>Add Task</button>
+          <br/><br/>
+          <div onChange={changeRadio}>
+              <input type='radio'  name='status' value='all' /> &nbsp;All &nbsp; &nbsp; &nbsp;
+              <input type='radio'  name='status' value='true'/> &nbsp;Completed &nbsp; &nbsp; &nbsp;
+              <input type='radio' name='status'  value='false'/> &nbsp; Not Completed<br/><br/> 
+          </div>
           <div>{errmsg}</div>
           <br/>
       </form>
      
-      <ShowTasks ar={ar}></ShowTasks>
+      <ShowTasks ar={ar} doneTask={doneTask} deleteTask={deleteTask} showOnly={showOnly} ></ShowTasks>
       </div>
   );
 }
--- React/sample/src/ShowTasks.jsx
+++ React/sample/src/ShowTasks.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 
 function ShowTasks(props) {
+  console.log(props)
+  var temp=props.ar.filter(e=>{
+        if(props.showOnly=='all')
+          return true
+        else if(e.status && props.showOnly=='true'){
+          console.log('show true tasks',e.status)
+          return true;
+        }
+        else if(!e.status && props.showOnly=='false'){
+          console.log('show false tasks',e.status)
+          return true;
+        }
+      })
   return (
     <div>
-        <ul>
-            {props.ar.map((e,i)=>{
-                return <li>{e.title}</li>
-            })}
-        </ul>
+        {temp.map((e,i)=>{
+            return <div key={i} style={{background: e.status ? '#88de88': 'pink'  ,marginBottom:'15px',padding:'10px'}}>
+                {e.title}&nbsp; &nbsp; &nbsp;
+                    <button onClick={()=>{
+                      props.doneTask(i)
+                    }}>Done</button>&nbsp; &nbsp; &nbsp;
+                    <button onClick={()=>{
+                      props.deleteTask(i)
+                    }}>Delete</button>
+              
+              </div>
+         })
+        }
       
     </div>
   )
