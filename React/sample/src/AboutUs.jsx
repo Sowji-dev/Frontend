80fb3ab4dfe26c6077650adb83064cfc8fb504e8
Sowji-dev <sowjanya.kadupu@gmail.com>
2023-09-06 09:49:18 +0530

    s

--- React/sample/src/AboutUs.jsx
+++ React/sample/src/AboutUs.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function AboutUs() {
+  var [bord,setBord]=useState(false)
+  function onmouseover(){
+    console.log('dfs')
+    setBord(!bord)
+  }
+  function onmouseleave(){
+    console.log('dfs')
+    setBord(false)
+  }
   return (
     <div>
       AboutUs
-    
+      <div style={bord ? {border:'2px solid green'} :  {border:'2px solid red'}}>
+        <h3>hello</h3>
+        <i class="fa fa-map-marker" aria-hidden="true" onMouseOver={onmouseover} onMouseLeave={onmouseover}></i>
+         </div>
     </div>
   )
 }
